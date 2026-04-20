03eddaeb5b92b43519da6a260371ed8190d3f0d0
Mehmood Deshmukh <meshde.md@gmail.com>
2026-04-20 11:12:43 +0400

    Update formula to v0.6.0

--- Formula/hit.rb
+++ Formula/hit.rb
@@ -1,34 +1,15 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Hit < Formula
-  desc ""
-  homepage ""
-  url "https://github.com/meshde/hit-cli/releases/download/v0.5.1/hit-mac.tar.gz"
-  sha256 "2142ddb291862c7f05bb05adfb8d0bcfa3ebef7fc7ca45d5652bcc4fb406e450"
+  desc "Productivity-focussed command-line API client"
+  homepage "https://github.com/meshde/hit-cli"
+  url "https://github.com/meshde/hit-cli/releases/download/v0.6.0/hit-mac.tar.gz"
+  sha256 "145417b1e119888368cc0532a312ee64e530e82c853e5af44bc0870ec99b4e19"
   license ""
-
-  uses_from_macos "ruby"
 
   def install
     bin.install "hit-cli" => "hit"
-    # ENV["GEM_HOME"] = libexec
-    # system "gem", "build", "#{name}.gemspec"
-    # system "gem", "install", "#{name}-#{@version}.gem"
-    # bin.install libexec/"bin/#{name}"
-    # bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test hit`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    system bin/"hit", "--version"
   end
 end
